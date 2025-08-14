7ebf04dbf44e4aadc171d512539558882ff4fe96
Frostleaft07 <90104112+Frostleaft07@users.noreply.github.com>
2025-08-14 22:52:09 +0700

    Revert "fs: notify: fdinfo: fix undeclare identifier"
    This reverts commit b9e68cdbf0352cfb2f52d3db1221e26923b8eb64.

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -121,7 +121,7 @@ static void inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
 			seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:0 ",
 			   inode_mark->wd, path.dentry->d_inode->i_ino, path.dentry->d_inode->i_sb->s_dev,
 			   //inotify_mark_user_mask(mark));
-			mask, mark->ignored_mask);
+			   mask, mark->ignored_mask);
 			show_mark_fhandle(m, path.dentry->d_inode);
 			seq_putc(m, '\n');
 			iput(inode);
@@ -132,15 +132,14 @@ out_free_pathname:
 			kfree(pathname);
 		}
 out_seq_printf:
-#else
+#endif
 		/*
 		 * IN_ALL_EVENTS represents all of the mask bits
 		 * that we expose to userspace.  There is at
 		 * least one bit (FS_EVENT_ON_CHILD) which is
 		 * used only internally to the kernel.
 		 */
-		u32 mask = mark->mask & IN_ALL_EVENTS;
-#endif
+		//u32 mask = mark->mask & IN_ALL_EVENTS;
 		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:%x ",
 			   inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
 			   mask, mark->ignored_mask);
